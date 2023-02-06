4995e9c58f82ce74b7e11f8c010038125ab7d113
karthickumar35 <karthickcs444@gmail.com>
2023-02-06 15:00:16 +0530

    update

---
+++ js/script-1.js
@@ -0,0 +1,322 @@
+/*------1------*/
+let home={
+	height:"30feet",
+	width:"40feet",
+	color:"white",
+	doornum:"4_3_15",
+	window:6,
+	door:2,
+}
+document.write(JSON.stringify(home));
+console.log(home);
+console.log(home.color);
+console.log(home["width"]);
+document.write("<br>");
+/*------2------*/
+let icecream={
+	name:"choco bar",
+	height:25,
+	width:10,
+	color:"brown",
+	amount:"rs:20",
+	flavour:"vennila",
+	shape:"triangle",
+}
+document.write(JSON.stringify(icecream));
+console.log(icecream);
+console.log(icecream.flavour);
+console.log(icecream["shape"]);
+document.write("<br>");
+
+/*------3------*/
+let bike={
+	name:"royal",
+	milage:350,
+	gear:5,
+	color:"blackred",
+	amount:225000,
+	speed:60,
+	fuel:"petrol",
+}
+document.write(JSON.stringify(bike));
+console.log(bike);
+console.log(bike.milage);
+console.log(bike["color"]);
+document.write("<br>");
+
+/*------4------*/
+let car={
+	name:"indica",
+	milage:25,
+	gear:5,
+	color:"white",
+	amount:405000,
+	seat:5,
+	fuel:"diesel",
+}
+document.write(JSON.stringify(car));
+console.log(car);
+console.log(car.seat);
+console.log(car["amount"]);
+document.write("<br>");
+
+/*------5------*/
+let remote={
+	name:"remote",
+	height:40,
+	width:5,
+	color:"black",
+	amount:100,
+	btn:35,
+	battery:2,
+}
+document.write(JSON.stringify(remote));
+console.log(remote);
+console.log(remote.amount);
+console.log(remote["name"]);
+document.write("<br>");
+
+/*------6------*/
+let sofa={
+	name:"sofa",
+	height:200,
+	width:50,
+	color:"blackred",
+	amount:35000,
+	seat:6,
+	seatspring:8,
+}
+document.write(JSON.stringify(sofa));
+console.log(sofa);
+console.log(sofa.color);
+console.log(sofa["amount"]);
+document.write("<br>");
+
+/*------7------*/
+let door={
+	name:"door",
+	height:500,
+	width:100,
+	color:"brown",
+	amount:8000,
+	lock:4,
+	models:3,
+}
+document.write(JSON.stringify(door));
+console.log(door);
+console.log(door.height);
+console.log(door["amount"]);
+document.write("<br>");
+
+/*------8------*/
+let clock={
+	name:"clock",
+	height:200,
+	width:200,
+	color:"blue",
+	amount:500,
+	part:3,
+	hour:24,
+}
+document.write(JSON.stringify(clock));
+console.log(clock);
+console.log(clock.hour);
+console.log(clock["name"]);
+document.write("<br>");
+
+/*------9------*/
+let shirt={
+	name:"shirt",
+	size:40,
+	quality:100,
+	color:"black",
+	amount:1700,
+	pocket:2,
+	btn:7,
+}
+document.write(JSON.stringify(shirt));
+console.log(shirt);
+console.log(shirt.btn);
+console.log(shirt["name"]);
+document.write("<br>");
+
+/*------10------*/
+let train={
+	name:"train",
+	length:500,
+	quantity:18,
+	color:"blue",
+	seat:1800,
+	engine:2,
+	driver:2,
+}
+document.write(JSON.stringify(train));
+console.log(train);
+console.log(train.quantity);
+console.log(train["driver"]);
+document.write("<br>");
+
+/*------11------*/
+let fan={
+	name:"fan",
+	size:36,
+	amount:5000,
+	color:"red",
+	leafs:3,
+	material:"ABS Plastic",
+	controls:5,
+}
+document.write(JSON.stringify(fan));
+console.log(fan);
+console.log(fan.amount);
+console.log(fan["material"]);
+document.write("<br>");
+
+/*------12------*/
+let boat={
+	name:"boat",
+	size:36,
+	amount:55000,
+	color:"brown",
+	speed:510,
+	material:"fibre-reinforced plastic",
+	controlsPanel:12,
+}
+document.write(JSON.stringify(boat));
+console.log(boat);
+console.log(boat.color);
+console.log(boat["material"]);
+document.write("<br>");
+
+
+/*------13------*/
+let book={
+	name:"book",
+	pages:460,
+	amount:550,
+	color:"green",
+	index:50,
+	material:"paper",
+	parts:8,
+}
+document.write(JSON.stringify(book));
+console.log(book);
+console.log(book.parts);
+console.log(book["material"]);
+document.write("<br>");
+
+/*------14------*/
+let phone={
+	name:"realme",
+	height:30,
+	width:10,
+	color:"shineblue",
+	amount:21000,
+	ram:5,
+	rom:124,
+}
+document.write(JSON.stringify(phone));
+console.log(phone);
+console.log(phone.amount);
+console.log(phone["name"]);
+document.write("<br>");
+
+/*------15------*/
+let cableBox={
+	name:"cableBox",
+	cableTypes:5,
+	amount:850,
+	color:"black",
+	size:22,
+	material:"corrugated sheets",
+	cableSBM:1000,
+}
+document.write(JSON.stringify(cableBox));
+console.log(cableBox);
+console.log(cableBox.cableTypes);
+console.log(cableBox["material"]);
+document.write("<br>");
+
+/*------16------*/
+let speaker={
+	name:"speaker",
+	quantity:5,
+	amount:10850,
+	color:"red",
+	size:8,
+	material:"aramid fiber",
+	modelName:"Zebronics Zeb-Warrior",
+}
+document.write(JSON.stringify(speaker));
+console.log(speaker);
+console.log(speaker.modelNname);
+console.log(speaker["material"]);
+document.write("<br>");
+
+/*------17------*/
+let laptop={
+	name:"laptop",
+	ScreenSize:13.5,
+	amount:199850,
+	color:"black",
+	CPUModel:"Core i7",
+	material:"aluminium",
+	weight:2,
+}
+document.write(JSON.stringify(laptop));
+console.log(laptop);
+console.log(laptop.CPUModel);
+console.log(laptop["material"]);
+document.write("<br>");
+
+/*------18------*/
+let candleHolder={
+	name:"candleHolder",
+    Size:18,
+	amount:1950,
+	color:"white",
+	nopiece:3,
+	material:"Wood",
+	weight:2,
+}
+document.write(JSON.stringify(candleHolder));
+console.log(candleHolder);
+console.log(candleHolder.amount);
+console.log(candleHolder["name"]);
+document.write("<br>");
+
+/*------19------*/
+let bed={
+	name:"bedtable",
+    Size:84,
+	amount:33950,
+	color:"white",
+    SpecialFeature:"firm, orthopedic, air vent",
+	material:"Wood",
+	modelName:"SmartGRID Ortho White"
+}
+document.write(JSON.stringify(bed));
+console.log(bed);
+console.log(bed.SpecialFeature);
+console.log(bed["model-name"]);
+document.write("<br>");
+
+/*------20------*/
+let vehicle={
+	name:"bus",
+    capacity:50,
+	stop:5,
+	color:"blue",
+	gear:6,
+	passenger:{
+		male:20,
+		female:30,
+	},
+	ticket:30,
+}
+document.write(JSON.stringify(vehicle));
+console.log(vehicle);
+console.log(vehicle.passenger.male);
+console.log(vehicle["ticket"]);
+document.write("<br>");
+
